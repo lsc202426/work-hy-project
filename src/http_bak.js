ea5faeb09a78eef74ea9db529430105aab843f5b
linshichao <563951731@qq.com>
2019-06-25 14:03:25 +0800

    token--chao

--- src/http_bak.js
+++ src/http_bak.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Qs from "qs";
-import { Indicator, Toast } from "mint-ui";
+import {Indicator} from 'mint-ui';
 
 if (process.env.NODE_ENV === "development") {
   // 设置默认本地axios提交url
@@ -11,53 +11,32 @@ if (process.env.NODE_ENV === "development") {
 }
 axios.defaults.headers.post["Content-Type"] =
   "application/x-www-form-urlencoded";
-
+	
 axios.interceptors.request.use(
   config => {
     if (config.method == "post") {
-      config.data = Qs.stringify(config.data); //防止post请求参数无法传到后台
+      config.data = config.data; //防止post请求参数无法传到后台
     }
 		config.data = {
 				...config.data,
 				access_token:sessionStorage.token
 		}
-		config.data = qs.stringify(config.data);
-    //请求loading
-    Indicator.open({
-      text: "加载中...",
-      spinnerType: "fading-circle"
-    });
+		config.data = Qs.stringify(config.data);
+		//请求loading
+		Indicator.open({
+			text: '加载中...',
+			spinnerType: 'fading-circle'
+		});
     return config;
   },
   error => {
     return Promise.reject(error);
   }
 );
-const that = this;
-// 响应拦截
-axios.interceptors.response.use(
-  function(res) {
-    // 如果账号在其他地方登陆
-    if (res.data.content.errcode === 10002) {
-      //  提示错误
-      Toast({
-        message: res.data.content.errmsg,
-        duration: 1500
-      });
-      setTimeout(function() {
-        sessionStorage.clear();
-        that.$router.replace({
-          path: "/login"
-        });
-      }, 1500);
-      return false;
-    }
-    //隐藏loading
-    Indicator.close();
-    return res;
-  },
-  function(error) {
-    return Promise.reject(error);
-  }
-);
+axios.interceptors.response.use(function(config){
+	//隐藏loading
+	Indicator.close();
+	//获取到config中的data,进行加工
+	return config;
+})
 export default axios;
