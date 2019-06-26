881a48af5ebc971a0cfaf4821dfa31fba3873051
liuquanhui <quanhui.liu@wjbrans.com>
2019-06-26 10:46:22 +0800

    --点商标分类 --加入购物清单

--- src/components/trademark/applyClass.vue
+++ src/components/trademark/applyClass.vue
@@ -88,6 +88,8 @@ export default {
       isLoading: false,
       // 大类选中
       classSelect: "",
+      // 大类名称
+      className: "",
       // 暂存当前子类选中
       isChildSelect: "",
       // 暂存当前子类数据
@@ -99,10 +101,7 @@ export default {
       allTypeClass: {},
       allItemArr: [],
       allPrice: 0,
-      temptShopCart: [],
-      big1: [],
-      mid: [],
-      small: []
+      temtpClass: {}
     };
   },
   mounted() {
@@ -137,7 +136,8 @@ export default {
           that.applyClass.map(function(item) {
             if (item.key === "01") {
               item.isSelect = true;
-              that.classSelect = item.name;
+              that.classSelect = item.key;
+              that.className = item.name;
             } else {
               item.isSelect = false;
             }
@@ -166,7 +166,8 @@ export default {
       that.isLoading = true;
       that.curList = [];
       // 记录当前选中大类
-      that.classSelect = item.name;
+      that.classSelect = item.key;
+      that.className = item.name;
       that.$axios
         .post("/index.php?c=App&a=getBsProductService", {
           bskey: item.key,
@@ -186,7 +187,6 @@ export default {
     // 切换小类
     switchCurList: function(item) {
       const that = this;
-      console.log(item);
       if (that.isChildSelect === item.categorycode) {
         that.isShow = !that.isShow;
       } else {
@@ -198,14 +198,13 @@ export default {
         that.itemArr = [];
       }
       if (that.temptCurList[item.categorycode]) {
-        console.log(item);
         that.isChildSelect = item.categorycode;
         return false;
       }
       that.$axios
         .post("/index.php?c=App&a=getBsProductService", {
           userid: 1,
-          bskey: that.classSelect.split("、")[0],
+          bskey: that.classSelect,
           keyword: "",
           pgroup: item.categorycode,
           productid: ""
@@ -232,9 +231,10 @@ export default {
     selectProduct: function(_item) {
       const that = this;
       _item.isSelect = !_item.isSelect;
-      console.log(this.classSelect + this.isChildSelect + _item.productid);
-      _item.TemptType = this.classSelect;
-      _item.TemptClass = this.isChildSelect;
+      let tempkey =
+        this.classSelect + "_" + this.isChildSelect + "_" + _item.productid;
+      let temptValue =
+        this.className + "_" + that.isChildSelect + "_" + _item.productname;
       // 数据结果太深，强制渲染
       this.$forceUpdate();
       // 是否删除
@@ -243,7 +243,11 @@ export default {
       if (_item.isSelect) {
         this.itemArr.push(_item);
         this.allItemArr.push(_item);
+        // 如果选中
+        that.temtpClass[tempkey] = temptValue;
       } else {
+        // 未选中
+        delete that.temtpClass[tempkey];
         that.temptSelect[this.isChildSelect].map(function(m, key) {
           if (_item.productid === m.productid) {
             that.temptSelect[that.isChildSelect].splice(key, 1);
@@ -252,10 +256,10 @@ export default {
             }
           }
         });
-        that.allTypeClass[this.classSelect].map(function(m, key) {
+        that.allTypeClass[this.className].map(function(m, key) {
           if (_item.productid === m.productid) {
-            that.allTypeClass[that.classSelect].splice(key, 1);
-            if (that.allTypeClass[that.classSelect].length < 1) {
+            that.allTypeClass[that.className].splice(key, 1);
+            if (that.allTypeClass[that.className].length < 1) {
               // 判断删除
               isDelete = true;
             }
@@ -268,9 +272,9 @@ export default {
         this.temptSelect[this.isChildSelect] = this.itemArr;
       }
       if (isDelete) {
-        delete that.allTypeClass[that.classSelect];
+        delete that.allTypeClass[that.className];
       } else {
-        this.allTypeClass[this.classSelect] = this.allItemArr;
+        this.allTypeClass[this.className] = this.allItemArr;
       }
       // 计算大类
       let len = Object.keys(this.allTypeClass).length;
@@ -292,146 +296,56 @@ export default {
       }
       // 总计，无年份
       this.allPrice = bigPrice + smallPrice;
-
-      // let temptProducts = [];
-      // temptProducts.push(that.temptSelect[that.isChildSelect]);
-      let samll1 = {
-        id: _item.productid,
-        name: _item.productname
-      };
-
-      let mid1 = {
-        code: this.isChildSelect,
-        products: samll1
-      };
-      let test = {
-        categoryName: this.classSelect,
-        detail: [mid1]
-      };
-      // console.log(test);
-      // console.log(this.classSelect, that.isChildSelect, _item);
-      if (that.temptShopCart.length < 1) {
-        // that.temptShopCart.push(test);
-        that.big1.push(test);
-      } else {
-        // for (let i = 0; i < that.temptShopCart.length; i++) {
-        //   if (that.temptShopCart[i].categoryName === that.classSelect) {
-        //     that.temptShopCart.splice(i, 1, test);
-        //     // that.temptShopCart.push(test);
-        //     // console.log(test);
-        //   }
-        // }
-        that.big1.push(test);
-      }
-      that.temptShopCart = that.big1;
-      // console.log(that.temptShopCart);
-
-      // for(let i=0;that.temptShopCart.length;i++){
-      //    if(){
-
-      //    }
-      // }
-
-      // console.log(that.temptShopCart);
-      // 组合临时清单数据
     },
     // 确认
     sureSelect: function() {
-      console.log(this.allTypeClass);
-
-      // let testArr = Object.keys(this.temptSelect);
-
-      // let tempErst = [];
-      // let small = [];
-      let mid = [];
-      let big = [];
-      // let t = {
-      //   "01_0101_12345": "01、化学品_0101_dgdg发给个",
-      //   "02_0101_12345": "02、生活品_0101_dgasd个",
-      //   "03_0102_12345": "03、学品_0102_d暗示的撒很简单"
-      // };
-      // let ts = [];
-      // let exts_l1 = [];
-      // let exts_l2 = [];
-      // for (let key in t) {
-      //   console.log(t[key]);
-      //   if(in_array(key.split('_')[0]), $exts_l1) {
-
-      //   }
-      //   let data = {
-      //     categoryName: t[key.split("_")[0]],
-      //     detail: [
-      //       {
-      //         code: t[key.split("_")[1]],
-      //         products: [
-      //           {
-      //             id: t[.split("_")[3]],
-      //             name: t[key].split("_")[2]
-      //           }
-      //         ]
-      //       }
-      //     ]
-      //   };
-      //   ts.push(data);
-      // }
-      // console.log(ts);
-      // 遍历数组
-      for (let key in this.allTypeClass) {
-        for (let val in this.temptSelect) {
-          for (let i = 0; i < this.temptSelect[val].length; i++) {
-            // console.log(
-            //   key,
-            //   val,
-            //   this.temptSelect[val][i].productid,
-            //   this.temptSelect[val][i].productname
-            // );
-
-            // let item3 = {
-            //   id: this.temptSelect[val][i].productid,
-            //   name: this.temptSelect[val][i].productname
-            // };
-            if (val === this.temptSelect[val][i].TemptClass) {
-              mid.push(this.temptSelect[val][i]);
-            }
-            let item2 = {
-              code: val,
-              products: mid
-            };
-            if (key === this.temptSelect[val][i].TemptType) {
-              big.push(item2);
-            }
-
-            // let item1 = {
-            //   categoryName: key,
-            //   detail: big
-            // };
-            // console.log(item1);
-            // // if(){
-            // let item3 = {
-            //   id: this.temptSelect[val][i].productid,
-            //   name: this.temptSelect[val][i].productname
-            // };
-            // mid.push(item3);
-            // let item2 = {
-            //   code: val,
-            //   products: [...new Set(mid)]
-            // };
-            // small.push(item2);
-
-            // let item = {
-            //   categoryName: key,
-            //   detail: [...new Set(small)]
-            // };
-            // tempErst.push(item);
-            // // }
-          }
+      var stuct = {};
+      var exts_key = ",";
+      var exts_group = ",";
+      for (var key in this.temtpClass) {
+        var keys = key.split("_");
+        var values = this.temtpClass[key].split("_");
+        if (exts_key.indexOf("," + keys[0] + ",") == -1) {
+          //大类不存在
+          var tmp = {
+            categoryName: values[0],
+            detail: {}
+          };
+          stuct[keys[0]] = tmp;
+          exts_key += keys[0] + ",";
         }
+        if (exts_group.indexOf("," + keys[1] + ",") == -1) {
+          //群组不存在
+          exts_group += keys[1] + ",";
+          stuct[keys[0]].detail[keys[1]] = {
+            name: keys[1],
+            products: []
+          };
+        }
+        var item = { id: keys[2], name: values[2] };
+        stuct[keys[0]].detail[keys[1]].products.push(item);
       }
-      // let set1 = new Set(tempErst);
-      // console.log(set1);
+      var result = [];
+      for (var i in stuct) {
+        var tmpObj = {
+          categoryName: stuct[i].categoryName,
+          detail: []
+        };
+        for (var j in stuct[i].detail) {
+          tmpObj.detail.push(stuct[i].detail[j]);
+        }
+        result.push(tmpObj);
+      }
+      let Test = {};
+      result.map(function(item) {
+        item.detail.map(function(_item) {
+          Test[item.categoryName] = _item.products;
+        });
+      });
       let _item = {
-        isShow: true,
-        content: this.allTypeClass,
+        isShow: false,
+        content: result,
+        classType: this.allTypeClass,
         allPrice: this.allPrice
       };
       this[MutationTypes.SET_SELECT_CLASS](_item);
