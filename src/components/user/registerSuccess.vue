34663bf094eeb4e9b0a5368246e161c971e0e6b8
liuquanhui <quanhui.liu@wjbrans.com>
2019-07-03 19:12:44 +0800

    --fix bug

--- src/components/user/registerSuccess.vue
+++ src/components/user/registerSuccess.vue
@@ -2,7 +2,7 @@
   <div class="register-success">
     <div class="icons"></div>
     <p>恭喜你，注册成功</p>
-    <router-link to="/register">
+    <router-link to="/login">
       <button>登录账号</button>
     </router-link>
   </div>
