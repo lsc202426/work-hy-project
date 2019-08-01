645673d01ec1049dfb5ace07113d2e47d87039e6
chenliyan <1318796090@qq.com>
2019-08-01 17:22:01 +0800

    申请人删除

--- src/components/subject/subjectList.vue
+++ src/components/subject/subjectList.vue
@@ -11,76 +11,81 @@
                 <div class="exp_txt">
                     按照工信部要求，域名申请人必须实名，若未实名会 被注册局暂停解析（Serverhold），请及时实名
                 </div>
-                <div class="subject_item" v-for="(list, index) in lists" :key="list.id" @click="editDetail(list.id, index)">
-                    <div class="subject_item_list">
-                        <span>申请人</span>
-                        <span
-                            >{{ list.corpname }}
-                            <img
-                                v-if="list.status == 0 || list.status == -1"
-                                @click.stop="verifyReal(list.id)"
-                                class="icon_verify_go"
-                                src="../../assets/images/subject/icon_real_go.png"
-                                alt=""
-                            />
-                            <i
-                                v-if="list.status"
-                                @click.stop="verifyReal(list.id)"
-                                class="list_status"
-                                :class="{ red: list.status == -1 || list.status == 0 }"
-                                >{{ list.status_name }}</i
-                            >
-                        </span>
-                    </div>
-                    <div class="subject_item_list">
-                        <span>联系人</span>
-                        <span>{{ list.linkman }}</span>
-                    </div>
-                    <div class="subject_item_list">
-                        <span>联系电话</span>
-                        <span>{{ list.mobile ? list.mobile : list.phone }}<i class="icon_right"></i></span>
-                    </div>
-                    <div class="subject_item_list">
-                        <span>邮箱</span>
-                        <span>
-                            {{ list.email }}
-                            <img
-                                v-if="list.verify_email == 0"
-                                @click.stop="verifyEmail(list.id, list.email)"
-                                class="icon_verify_go"
-                                src="../../assets/images/subject/icon_verify_go.png"
-                                alt=""
-                            />
-                            <i
-                                v-if="list.verify_email"
-                                class="list_status"
-                                :class="{ red: list.verify_email == -1 || list.verify_email == 0 }"
-                                >{{ list.verify_email_name }}</i
-                            >
-                            <!-- <img
-							    v-if="list.verify_email == 0"
-							    class="icon_verify"
-							    src="../../assets/images/subject/icon_verify_w.png"
-							    alt=""
-							/>
-							<img v-else class="icon_verify" src="../../assets/images/subject/icon_verify_y.png" alt="" /> -->
-                        </span>
-                    </div>
-                    <div class="subject_item_list">
-                        <span>详细地址</span>
-                        <span>{{ list.province }}{{ list.city }}{{ list.area }}{{ list.address }}</span>
-                    </div>
-                    <!-- <div class="subject_item_list">
-                        <span>审核状态</span>
-                        <span
-                            :class="{
-                                status_d: list.status_name == '待审核',
-                                status_y: list.status_name == '已审核',
-                                status_w: list.status_name == '未通过',
-                            }"
-                            >{{ list.status_name }}</span
-                        >
-                    </div> -->
+                <div class="subject_item" v-for="(list, index) in lists" :key="list.id" >
+                    <deleted @deleteItem="deleteItem(list.id)" :index="index">
+                        <div class="list-dubject"  @click.stop="editDetail(list.id, index)">
+
+                            <div class="subject_item_list">
+                                <span>申请人</span>
+                                <span
+                                    >{{ list.corpname }}
+                                    <img
+                                        v-if="list.status == 0 || list.status == -1"
+                                        @click.stop="verifyReal(list.id)"
+                                        class="icon_verify_go"
+                                        src="../../assets/images/subject/icon_real_go.png"
+                                        alt=""
+                                    />
+                                    <i
+                                        v-if="list.status"
+                                        @click.stop="verifyReal(list.id)"
+                                        class="list_status"
+                                        :class="{ red: list.status == -1 || list.status == 0 }"
+                                        >{{ list.status_name }}</i
+                                    >
+                                </span>
+                            </div>
+                            <div class="subject_item_list">
+                                <span>联系人</span>
+                                <span>{{ list.linkman }}</span>
+                            </div>
+                            <div class="subject_item_list">
+                                <span>联系电话</span>
+                                <span>{{ list.mobile ? list.mobile : list.phone }}<i class="icon_right"></i></span>
+                            </div>
+                            <div class="subject_item_list">
+                                <span>邮箱</span>
+                                <span>
+                                    {{ list.email }}
+                                    <img
+                                        v-if="list.verify_email == 0"
+                                        @click.stop="verifyEmail(list.id, list.email)"
+                                        class="icon_verify_go"
+                                        src="../../assets/images/subject/icon_verify_go.png"
+                                        alt=""
+                                    />
+                                    <i
+                                        v-if="list.verify_email"
+                                        class="list_status"
+                                        :class="{ red: list.verify_email == -1 || list.verify_email == 0 }"
+                                        >{{ list.verify_email_name }}</i
+                                    >
+                                    <!-- <img
+                                        v-if="list.verify_email == 0"
+                                        class="icon_verify"
+                                        src="../../assets/images/subject/icon_verify_w.png"
+                                        alt=""
+                                    />
+                                    <img v-else class="icon_verify" src="../../assets/images/subject/icon_verify_y.png" alt="" /> -->
+                                </span>
+                            </div>
+                            <div class="subject_item_list">
+                                <span>详细地址</span>
+                                <span>{{ list.province }}{{ list.city }}{{ list.area }}{{ list.address }}</span>
+                            </div>
+                            <!-- <div class="subject_item_list">
+                                <span>审核状态</span>
+                                <span
+                                    :class="{
+                                        status_d: list.status_name == '待审核',
+                                        status_y: list.status_name == '已审核',
+                                        status_w: list.status_name == '未通过',
+                                    }"
+                                    >{{ list.status_name }}</span
+                                >
+                            </div> -->
+                        </div>
+                    </deleted>
                 </div>
             </div>
         </div>
@@ -99,6 +104,10 @@ import blankPage from '@/components/order/blankPage.vue';
 import * as GetterTypes from '@/constants/GetterTypes';
 import * as MutationTypes from '@/constants/MutationTypes';
 import { mapGetters, mapMutations } from 'vuex';
+import { Toast, Indicator } from 'mint-ui';
+
+import deleted from '@/components/commom/deleted.vue';
+
 export default {
     name: 'subjectList',
     data() {
@@ -109,10 +118,12 @@ export default {
     },
     components: {
         blankPage,
+        deleted
     },
     created() {
         this.init();
     },
+    
     computed: {
         ...mapGetters([[GetterTypes.GET_APPLY_INFOR]]),
         ...mapGetters({
@@ -124,6 +135,34 @@ export default {
         ...mapMutations({
             [MutationTypes.SET_APPLY_INFOR]: MutationTypes.SET_APPLY_INFOR,
         }),
+        // 删除申请人
+        deleteItem(index){
+            var _this = this;
+            console.log(index)
+             _this.$axios.post('index.php?c=App&a=delSubject',{
+                id: index
+                
+            }).then(function(response) {
+                console.log(response)
+                if(response.data.errcode == 0){
+                    Toast({
+                        message: '删除成功',
+                        duration: 2000,
+                    });
+                    setTimeout(() => {
+                        if (response.data.errcode == 0) {
+                            _this.$router.go(0)
+                            // _this.lists = response.data.content;
+                        }
+                    }, 3000);
+                }else{
+                    Toast({
+                        message: response.data.errmsg,
+                        duration: 2000,
+                    });
+                }
+            });
+        },
         init() {
             let _this = this;
             _this.$axios.post('index.php?c=App&a=getRegisterSubject').then(function(response) {
@@ -195,4 +234,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+    .list-dubject{
+        z-index: 1;
+        background: #fff;
+    }
+</style>
