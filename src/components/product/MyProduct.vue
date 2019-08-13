e2a20770a7cb5a545ddc1a73d823d1c0ebdf94c0
liuquanhui <quanhui.liu@wjbrans.com>
2019-08-13 17:36:50 +0800

    --我的产品-链接

--- src/components/product/MyProduct.vue
+++ src/components/product/MyProduct.vue
@@ -5,7 +5,7 @@
         <nar-list></nar-list>
         <div class="my-product-main containerView-main">
             <div class="my-product-list">
-                <div class="my-product-list-item" v-for="(item, index) of productList" :key="index" @click="viewDetail">
+                <div class="my-product-list-item" v-for="(item, index) of productList" :key="index" @click="viewDetail(item.mark)">
                     <img :src="item.icons" class="icons" />
                     <div class="right">
                         <div class="top">
@@ -32,31 +32,37 @@ export default {
                     icons: require('@/assets/images/myproduct/icon_sb1.png'),
                     name: '互易阿萨达撒旦撒旦撒多撒大萨达撒旦撒旦撒多撒多撒.商标',
                     endTime: '2019/08/09 14:07',
+                    mark: 'tmd',
+                },
+                {
+                    icons: require('@/assets/images/myproduct/icon_sb1.png'),
+                    name: '互易.招聘',
+                    endTime: '2019/08/09 14:07',
+                    mark: 'dzp',
                 },
                 {
                     icons: require('@/assets/images/myproduct/icon_sb1.png'),
                     name: '互易.商标',
                     endTime: '2019/08/09 14:07',
+                    mark: 'bs',
                 },
                 {
                     icons: require('@/assets/images/myproduct/icon_sb1.png'),
-                    name: '互易.商标',
+                    name: '互易.餐厅',
                     endTime: '2019/08/09 14:07',
+                    mark: 'dct',
                 },
                 {
                     icons: require('@/assets/images/myproduct/icon_sb1.png'),
-                    name: '互易.商标',
+                    name: '互易.com',
                     endTime: '2019/08/09 14:07',
+                    mark: 'domain',
                 },
                 {
                     icons: require('@/assets/images/myproduct/icon_sb1.png'),
-                    name: '互易.商标',
+                    name: '互易.小程序',
                     endTime: '2019/08/09 14:07',
-                },
-                {
-                    icons: require('@/assets/images/myproduct/icon_sb1.png'),
-                    name: '互易.商标',
-                    endTime: '2019/08/09 14:07',
+                    mark: 'ecweb',
                 },
                 {
                     icons: require('@/assets/images/myproduct/icon_sb1.png'),
@@ -100,22 +106,38 @@ export default {
             let typeList = [
                 { name: '全部', key: 0, mark: 'tmd' },
                 { name: '点商标', key: 1, mark: 'tmd' },
-                { name: '点招聘', key: 2, mark: 'tmd' },
-                { name: '点餐厅', key: 3, mark: 'tmd' },
-                { name: '商标', key: 4, mark: 'tmd' },
-                { name: '域名', key: 5, mark: 'tmd' },
-                { name: '网站', key: 6, mark: 'tmd' },
+                { name: '点招聘', key: 2, mark: 'dzp' },
+                { name: '点餐厅', key: 3, mark: 'dct' },
+                { name: '商标', key: 4, mark: 'bs' },
+                { name: '域名', key: 5, mark: 'domain' },
+                { name: '网站', key: 6, mark: 'ecweb' },
             ];
             this[MutationTypes.SET_NAR_LIST](typeList);
         },
         // 查看详情
-        viewDetail: function() {
-            this.$router.push({
-                path: '/productone',
-                query: {
-                    mark: 'tmd',
-                },
-            });
+        viewDetail: function(type) {
+            if (type === 'dzp' || type === 'tmd') {
+                this.$router.push({
+                    path: '/productone',
+                    query: {
+                        mark: type,
+                    },
+                });
+            } else if (type === 'bs') {
+                this.$router.push({
+                    path: '/productBs',
+                    query: {
+                        mark: type,
+                    },
+                });
+            } else if (type === 'ecweb') {
+                this.$router.push({
+                    path: '/productSite',
+                    query: {
+                        mark: type,
+                    },
+                });
+            }
         },
     },
     created() {
