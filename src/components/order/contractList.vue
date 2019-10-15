1c7a024dba44aaae400076c6cfff2a181749f5b4
linshichao <563951731@qq.com>
2019-10-15 18:01:47 +0800

    申请列表，证书，订单列表，评价等页面修改

--- src/components/order/contractList.vue
+++ src/components/order/contractList.vue
@@ -1,24 +1,27 @@
 <template>
     <div class="contractList bg_gray">
         <nav-header :title="title"></nav-header>
-        <div class="containerView-main">
-            <div class="list_content">
+        <div class="containerView-main"
+            v-infinite-scroll="loadMore"
+            infinite-scroll-disabled="moreLoading"
+            infinite-scroll-distance="10">
+            <div class="list_content" v-if="datas && datas.length > 0">
                 <div class="list_content_box">
-                    <div class="list_content_item f_bgf">
+                    <div class="list_content_item f_bgf" v-for="(item,index) in datas" :key="index">
                         <div v-if="!$route.query.past" class="item_left">
-                            <i :class="['checkbox_i',{active:isChecked}]" @click="setChecked()"></i>
+                            <i :class="['checkbox_i',{active:item.isChecked}]" @click="setChecked(item)"></i>
                         </div>
                         <div class="item_right">
-                            <div class="item_right_order" @click.stop="seeOrder()">
-                                <div class="item_num">HP23948942342394234234</div>
+                            <div class="item_right_order" @click.stop="seeOrder(item.order_no)">
+                                <div class="item_num">{{item.order_no}}</div>
                                 <div class="item_see">查看订单</div>
                             </div>
                             <div class="item_right_con">
                                 <div class="item_right_con_main">
-                                    <span class="con_main_name">广东互易网络知识产权有限公司</span>
-                                    <span class="con_main_money">￥12125.00</span>
+                                    <span class="con_main_name">{{item.corp_name}}</span>
+                                    <span class="con_main_money">￥{{item.total}}</span>
                                 </div>
-                                <div class="item_right_con_time">2019/03/08 12：01</div>
+                                <div class="item_right_con_time">{{item.created_time}}</div>
                             </div>
                             <!-- <div class="item_right_oper">
                                 <span>去开票</span>
@@ -30,23 +33,100 @@
                     申领
                 </div>
             </div>
+            <!-- 暂无数据 -->
+            <blankPage v-else></blankPage>
+            <!-- 加载更多 -->
+            <div class="load-more" v-show="moreLoading || allLoaded">
+                <p v-show="moreLoading" class="load-more-loading">
+                    <mt-spinner type="fading-circle"></mt-spinner>
+                </p>
+                <p class="load-more-no" v-show="allLoaded">已加载全部</p>
+            </div>
         </div>
     </div>
 </template>
 <script>
+import * as GetterTypes from '@/constants/GetterTypes';
+import * as MutationTypes from '@/constants/MutationTypes';
+import { mapGetters, mapMutations } from 'vuex';
 export default {
     name:"contractList",
     data() {
         return {
             title:this.$route.query.past?"历史合同":"申领合同",
             isChecked:false,
+            datas:[],
+            page: 1,// 当前分页
+            moreLoading: false,// 是否加载更多加载中
+            allLoaded: false,// 是否已加载全部
+            order_nos:[],//订单号
         }
     },
     created() {
-        
+        this.getList();//初始化获取数据
+    },
+    computed: {
+        ...mapGetters([[GetterTypes.GET_NAR_LIST], [GetterTypes.GET_IS_SELECT]]),
+        ...mapGetters({
+            getNarList: [GetterTypes.GET_NAR_LIST],
+            getIsSelect: [GetterTypes.GET_IS_SELECT],
+        }),
+    },
+    watch: {
+        getIsSelect: function() {
+            this.datas = [];
+            this.page = 1;
+            this.allLoaded = false;
+            this.moreLoading = false;
+            this.getList(this.getIsSelect.status, this.page);
+        },
     },
     methods: {
-        seeOrder(){
+        ...mapMutations([[MutationTypes.SET_NAR_LIST]]),
+        ...mapMutations({
+            [MutationTypes.SET_NAR_LIST]: MutationTypes.SET_NAR_LIST,
+        }),
+        //初始化获取数据
+        getList(status, page) {
+            let that = this;
+            this.$axios
+                .post('index.php?c=App&a=getContractOrInvoiceList', {
+                    type:1,
+                    history:that.$route.query.past?1:0,
+                    p: that.page,
+                })
+                .then(function(response) {
+                    let _data = response.data;
+                    if (_data.errcode == 0) {
+                        // 关闭加载更多
+                        that.moreLoading = false;
+                        //分页数据
+                        if (page <= 1) {
+                            that.datas = _data.content.list;
+                        } else {
+                            for (let i = 0; i < _data.content.list.length; i++) {
+                                that.datas.push(_data.content.list[i]);
+                            }
+                        }
+                        if (that.datas && that.datas.length > 0) {
+                            //判断是否加载完了
+                            if (_data.content.counter < _data.content.pgsize) {
+                                that.allLoaded = true;
+                            }
+                        }
+                        that.addIsChecked();
+                        
+                    }
+                });
+        },
+        //初始数据添加参数
+        addIsChecked(){
+            this.datas.forEach((item,index)=>{
+                item['isChecked']=false;
+            })
+            console.log(this.datas);
+        },
+        seeOrder(id){
             if(this.$route.query.past){
                 this.$router.push({
                     path:'/contDetail',
@@ -61,9 +141,21 @@ export default {
                 }
             })
         },
-        setChecked(){
-            this.isChecked=!this.isChecked;
-        }
+        setChecked(item){
+            console.log(item);
+            item.isChecked=!item.isChecked;
+        },
+        // 加载更多
+        loadMore: function() {
+            const that = this;
+            if (that.moreLoading === false && that.allLoaded === false && that.datas && that.datas.length > 0) {
+                that.moreLoading = true;
+                setTimeout(function() {
+                    that.page = that.page + 1;
+                    that.getList(that.getIsSelect.status, that.page);
+                }, 2500);
+            }
+        },
     },
 }
 </script>
--- src/components/order/evaluate.vue
+++ src/components/order/evaluate.vue
@@ -11,28 +11,28 @@
                     <span>整体评价</span>
                     <div class="bruce flex-ct-x">
                         <div class="star-rating">
-                            <input type="radio" name="rate">
-                            <input type="radio" name="rate">
-                            <input type="radio" name="rate">
-                            <input type="radio" name="rate">
-                            <input type="radio" name="rate">
+                            <input type="radio" @click="evaluateI(index)" name="rate" v-for="(item,index) in 5" :key="index">
                         </div>
                     </div>
-                    <span class="bruce_txt"></span>
+                    <span class="bruce_txt">{{bruceTxt}}</span>
                 </div>
                 <div class="evaluate_txt">
-                    <textarea name="" placeholder="分享你的心得" id=""></textarea>
+                    <textarea v-model="evaluateTxt" name="" placeholder="分享你的心得" id=""></textarea>
                 </div>
                 <div class="evaluate_file">
                     <div class="evaluate_img">
-                        <img src="../../assets/images/user/phone.png" alt="">
-                        <p>添加图片</p>
-                        <input type="file" accept="image/*" name="" id="">
+                        <div v-if="imgSrc" class="evaluate_img_bg">
+                            <div @click="delFile()" class="del_bg"></div>
+                            <img :src="configs.api.public_domain+imgSrc" alt="">
+                        </div>
+                        <input type="file" @change="toBase64($event)" accept="image/*" name="" id="">
                     </div>
                     <div class="evaluate_video">
-                        <img src="../../assets/images/user/phone.png" alt="">
-                        <p>添加视频</p>
-                        <input type="file" accept="video/*" name="" id="">
+                        <div v-if="videoSrc" class="evaluate_video_bg">
+                            <div @click="delFile('video')" class="del_bg"></div>
+                            <video :src="configs.api.public_domain+videoSrc" controls="controls"></video>
+                        </div>
+                        <input type="file" @change="toBase64($event,'video')" accept="video/*" name="" id="">
                     </div>
                 </div>
                 <div class="anonymous">
@@ -53,7 +53,11 @@ export default {
     name:"evaluate",
     data() {
         return {
-            isAnonymous:false
+            isAnonymous:false,
+            imgSrc:"",
+            videoSrc:"",
+            bruceTxt:"",
+            evaluateTxt:"",
         }
     },
     created() {
@@ -68,6 +72,69 @@ export default {
         },
         goback(){
             this.$router.go(-1);
+        },
+        //上传文件处理
+        toBase64(e,i){
+            let _this = this;
+            _this.isUpload = false;
+            let files = e.target.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(files);
+            reader.onload = function() {
+                if(i&&i==="video"){
+                    let imgcode = this.result.replace(/^data:image\/(mp4|MPEG4|WebM|ogg);base64,/, '');
+                }else{
+                    let imgcode = this.result.replace(/^data:image\/(jpeg|png|gif|jpg|bmp);base64,/, '');
+                }
+                let imgcode = this.result.replace(/^data:image\/(jpeg|png|gif|jpg|bmp);base64,/, '');
+                console.log(this);
+                _this.$axios
+                    .post('index.php?c=App&a=uploadAttachment', {
+                        filename: files.name,
+                        file_base64: imgcode,
+                    })
+                    .then(function(res) {
+                        if(res.data.errcode==0){
+                            if(i||i==="video"){
+                                _this.videoSrc=res.data.content.url;
+                            }else{
+                                _this.imgSrc=res.data.content.url;
+                            }
+                        }
+                    });
+                
+            }
+        },
+        //删除文件
+        delFile(i){
+            if(i&&i==="video"){
+                this.videoSrc="";
+            }else{
+                this.imgSrc="";
+            }
+        },
+        //选择评价星数
+        evaluateI(i){
+            let _this=this;
+            switch (i) {
+                case 0:
+                    _this.bruceTxt="非常好";
+                    break;
+                case 1:
+                    _this.bruceTxt="很好";
+                    break;
+                case 2:
+                    _this.bruceTxt="好";
+                    break;
+                case 3:
+                    _this.bruceTxt="一般";
+                    break;
+                case 4:
+                    _this.bruceTxt="差";
+                    break;
+                default:
+                    break;
+            }
         }
     },
 
