a9e35a72822a04c240f2999b0e3c6f661194384a
liuquanhui <quanhui.liu@wjbrans.com>
2019-11-04 10:03:38 +0800

    --修复选择注册人资料bug

--- src/components/user/material.vue
+++ src/components/user/material.vue
@@ -1,10 +1,8 @@
 <template>
     <div id="material" class="material">
-        <!-- <nav-header title="注册资料管理"></nav-header> -->
         <mt-header title="注册资料管理" class="header" fixed>
             <mt-button slot="left" icon="back" @click="$router.back(-1)"></mt-button>
             <mt-button icon="more" slot="right" @click="selectInfo()" v-if="isselect">{{ isCanSelect ? '取消' : '选择' }}</mt-button>
-            <!-- <mt-button icon="more" slot="right" @click="change('1')" v-show="!checkAll">全选</mt-button> -->
         </mt-header>
         <div class="containerView-main" v-if="getMsgArr.length != 0">
             <div class="tips">
@@ -17,7 +15,6 @@
                 <div class="capiral-box">
                     <ul>
                         <li v-for="item in getMsgArr" @click="selectItem(item)" :key="item.id">
-                            <!-- <input type="checkbox" v-show="!checkAll" /> -->
                             <i class="select-icons" v-if="isCanSelect" :class="{ active: item.isActive }"></i>
                             <img class="mater-img" :src="configs.api.public_domain + item.fileurl" alt="" />
                         </li>
@@ -41,13 +38,19 @@ export default {
 
     data() {
         return {
-            checkAll: false,
+            // 页码
             page: 1,
+            // 获取图片信息
             getMsgArr: [],
+            // 是否可選擇
             isselect: this.$route.query.isselect,
+            // 最多选择张数
             len: this.$route.query.num,
+            // 选择图片，保存的用途
             type: this.$route.query.type,
+            // 选择取消
             isCanSelect: false,
+            // 选择数据
             selectArr: [],
         };
     },
@@ -58,7 +61,6 @@ export default {
         // 保存图片
         saveImg() {
             const that = this;
-
             let temptArr = [];
             if (this.selectArr.length > 0) {
                 that.selectArr.map(function(item) {
@@ -89,7 +91,10 @@ export default {
         // 选择图片
         selectItem: function(item) {
             const that = this;
-
+            // 如果条件不成立，则不能选择
+            if (!that.isselect || !that.isCanSelect) {
+                return false;
+            }
             // 置反
             item.isActive = !item.isActive;
             // 如果数组长度小于所需长度
