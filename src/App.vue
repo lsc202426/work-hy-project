15c16a2ae2af5f30833c74d303d4b1b6c6164054
liuquanhui <quanhui.liu@wjbrans.com>
2019-12-23 16:33:22 +0800

    --禁止页面缩放

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,33 @@ export default {
             });
         },
     },
+    mounted() {
+        this.$nextTick(() => {
+            // 禁止双指缩放
+            document.documentElement.addEventListener(
+                'touchstart',
+                function(event) {
+                    if (event.touches.length > 1) {
+                        event.preventDefault();
+                    }
+                },
+                false
+            );
+            // 禁止双击缩放
+            var lastTouchEnd = 0;
+            document.documentElement.addEventListener(
+                'touchend',
+                function(event) {
+                    var now = Date.now();
+                    if (now - lastTouchEnd <= 300) {
+                        event.preventDefault();
+                    }
+                    lastTouchEnd = now;
+                },
+                false
+            );
+        });
+    },
     // created() {
     //     let u = navigator.userAgent;
     //     if (u.indexOf('iPhone') > -1) {
