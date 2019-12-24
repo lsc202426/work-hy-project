e0008bd4f72b7854c2364d94a67be72ba5fc1801
liuquanhui <quanhui.liu@wjbrans.com>
2019-12-24 19:13:02 +0800

    --去除跳转路由动画

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="app">
-        <transition name="fade" mode="out-in">
-            <router-view v-if="isRouterAlive" />
-        </transition>
+        <!-- <transition name="fade" mode="out-in"> -->
+        <router-view v-if="isRouterAlive" />
+        <!-- </transition> -->
     </div>
 </template>
 
 <style lang="scss">
 @import '@/assets/css/app.scss';
-@import '../static/font/font.css';
+// @import '../static/font/font.css';
 </style>
 <script>
 export default {
@@ -30,32 +30,32 @@ export default {
             });
         },
     },
-    // mounted() {
-    //     this.$nextTick(() => {
-    //         // 禁止双指缩放
-    //         document.documentElement.addEventListener(
-    //             'touchstart',
-    //             function(event) {
-    //                 if (event.touches.length > 1) {
-    //                     event.preventDefault();
-    //                 }
-    //             },
-    //             false
-    //         );
-    //         // 禁止双击缩放
-    //         var lastTouchEnd = 0;
-    //         document.documentElement.addEventListener(
-    //             'touchend',
-    //             function(event) {
-    //                 var now = Date.now();
-    //                 if (now - lastTouchEnd <= 300) {
-    //                     event.preventDefault();
-    //                 }
-    //                 lastTouchEnd = now;
-    //             },
-    //             false
-    //         );
-    //     });
-    // },
+    mounted() {
+        this.$nextTick(() => {
+            // 禁止双指缩放
+            document.documentElement.addEventListener(
+                'touchstart',
+                function(event) {
+                    if (event.touches.length > 1) {
+                        event.preventDefault();
+                    }
+                },
+                false
+            );
+            // 禁止双击缩放
+            var lastTouchEnd = 0;
+            document.documentElement.addEventListener(
+                'touchend',
+                function(event) {
+                    var now = Date.now();
+                    if (now - lastTouchEnd <= 300) {
+                        event.preventDefault();
+                    }
+                    lastTouchEnd = now;
+                },
+                false
+            );
+        });
+    },
 };
 </script>
