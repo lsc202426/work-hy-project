e260ccb6168b4d958a6a472b311abb9c6380a04d
liuquanhui <quanhui.liu@wjbrans.com>
2020-01-10 16:16:55 +0800

    --改回product_name

--- src/components/shoppingCart/shoppingCart.vue
+++ src/components/shoppingCart/shoppingCart.vue
@@ -63,13 +63,16 @@
                                     </span>
                                     <i class="icon_b"></i>
                                 </span>
-                                <span v-else>{{
-                                    item.item_mark == 'TMD_F1'
-                                        ? '全类别保护-独创品牌'
-                                        : item.item_mark == 'TMD_F2'
-                                        ? '全类别保护-知名品牌'
-                                        : ''
-                                }}</span>
+                                <span v-else>
+                                    {{ item.product_name }}
+                                    <!-- {{
+                                        item.item_mark == 'TMD_F1'
+                                            ? '全类别保护-独创品牌'
+                                            : item.item_mark == 'TMD_F2'
+                                            ? '全类别保护-知名品牌'
+                                            : ''
+                                    }} -->
+                                </span>
                             </p>
                             <!-- 商标 -->
                             <p v-if="item.product_mark == 'bs' && (item.feetype == 'Z' || item.feetype == 'X')" class="item_category">
